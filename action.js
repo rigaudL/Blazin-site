0e5d5b83b6e8ed0992b990c6ed51f45c4b95dd52
Rigaud Luly <RigaudLuly21@gmail.com>
2024-04-24 14:52:53 -0400

    project final

--- action.js
+++ action.js
@@ -1,5 +1,25 @@
-function type(){
+// // function sendemail(){
  
-   let textbox = document.querySelector("forjs");
-   
-}
+// //    let data = document.querySelector("useremails");
+// //    data.onc
+// // }
+
+// document.getElementById('sendemail').onclick = function(){
+//    useremails =  document.getElementById('useremail').value;
+//    usermessage = document.getElementById('usermessage').value;
+
+// }
+
+// console.log(useremails)
+
+document.getElementById('sendemail').onclick = function() {
+   let useremail = document.getElementById('useremails').value;
+   let usermessage = document.getElementById('usermessage').value;
+
+   console.log("User Email:", useremail);
+   console.log("User Message:", usermessage);
+
+   // Here you can add your email sending logic using the retrieved values
+   // For example, you can use JavaScript to send an HTTP request to a server-side script that handles email sending.
+   // Or you can use a service like EmailJS to send emails directly from the client-side.
+};
